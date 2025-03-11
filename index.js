06bae800e3850ad65da727462429bb7d063a4a2a
HilaryGH <hilarygebremedhn28@gmail.com>
2025-03-11 10:00:23 +0300

    Updated timestamp handling and fixed output format

--- index.js
+++ index.js
@@ -2,33 +2,34 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const PROJECT_URL = "https://timestamp-microservice-rzkn.onrender.com/";
+
 // Serve a simple HTML response
 app.get('/', (req, res) => {
-    res.send(`
-      <h1 style="color:#3d3d3d; font-size: 24px; padding:6px; text-align: center;">
-        Timestamp Microservice
-      </h1>
-      <hr>
-      <h2 style="color:#3d3d3d; font-size: 20px; text-align: center;">
-        Example Usage:
-      </h2>
-      <p style="text-align: center; font-size:14px;">
-        <a href="${PROJECT_URL}/api/date/2025-03-10" target="_blank" style="color:lightblue;">
-          ${PROJECT_URL} /api/date/2025-03-10
-        </a>
-      </p>
-      <p style="text-align: center; font-size:14px;">
-        <a href="${PROJECT_URL}/api/date" target="_blank" style="color:lightblue;">
-          ${PROJECT_URL}/api/date
-        </a>
-      </p>
-      <h2 style="color:#3d3d3d; font-size: 20px; text-align: center;">
-        Example Output:
-      </h2>
-      <p style="font-size:14px;text-align:center;">
-        {"unix":1741609132895,"utc":"Mon, 10 Mar 2025 12:18:52 GMT"}
-      </p>
-    `);
+  res.send(`
+    <h1 style="color:#3d3d3d; font-size: 24px; padding:6px; text-align: center;">
+      Timestamp Microservice
+    </h1>
+    <hr>
+    <h2 style="color:#3d3d3d; font-size: 20px; text-align: center;">
+      Example Usage:
+    </h2>
+    <p style="text-align: center; font-size:14px;">
+      <a href="${PROJECT_URL}/api/date/2025-03-10" target="_blank" style="color:lightblue;">
+        ${PROJECT_URL}/api/date/2025-03-10
+      </a>
+    </p>
+    <p style="text-align: center; font-size:14px;">
+      <a href="${PROJECT_URL}/api/date" target="_blank" style="color:lightblue;">
+        ${PROJECT_URL}/api/date
+      </a>
+    </p>
+    <h2 style="color:#3d3d3d; font-size: 20px; text-align: center;">
+      Example Output:
+    </h2>
+    <p style="font-size:14px;text-align:center;">
+      {"unix":1741609132895,"utc":"Mon, 10 Mar 2025 12:18:52 GMT"}
+    </p>
+  `);
 });
 
 // API endpoint for timestamp conversion using '/api/date/:date_string?' (date_string is optional)
